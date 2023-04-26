c92a282468c6dd2f927b3ac9871652af386a29a5
KristerJLawlor <kristerjlawlor@gmail.com>
2023-04-25 23:06:11 -0400

    did some scriptin

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject SpawnPoint;
+    public GameObject StartPoint;
+
+    public Text Score;
+    public Text Timer;
+    public float TimeLeft = 120.0f;
+    public int PlayerScore = 0;
+
+    public bool TimesUp = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = SpawnPoint.transform.position;
+        Score.text = "0";
+        Timer.text = "2:00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TimesUp)
+        {
+            GameObject.Find("Lose").SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Door")
+        {
+            //Move player indoors (No returning outside)
+            this.transform.position = StartPoint.transform.position;
+
+            //Start the timer
+            StartCoroutine(Countdown());
+
+        }
+        if (other.gameObject.tag == "Window")
+        {
+            //Will end the game and show a new overlay depending on score
+            if(PlayerScore >= 5)
+            {
+                GameObject.Find("Win").SetActive(true);
+            }
+            else
+            {
+                GameObject.Find("Poor").SetActive(true);
+            }
+
+        }
+
+        if (other.gameObject.tag == "Money")
+        {
+            PlayerScore++;
+            Score.text = "Score: " + PlayerScore.ToString();
+
+            Destroy(other.gameObject);
+        }
+
+    }
+
+    public IEnumerator Countdown()
+    {
+        while(TimeLeft > 0)
+        {
+            Timer.text = "Time: " + TimeLeft.ToString();
+            --TimeLeft;
+            yield return new WaitForSecondsRealtime(1.0f);
+            
+        }
+        Timer.text = TimeLeft.ToString();
+        TimesUp = true;
+    }
+}
